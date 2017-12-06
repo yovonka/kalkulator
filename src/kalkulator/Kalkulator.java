2a0cbb9bf007a31b0a2f619b4c9fd0c2bacf4be6
yovonka <iovonkagawel@gmail.com>
2017-12-06 18:59:31 +0100

    new approach

--- src/kalkulator/Kalkulator.java
+++ src/kalkulator/Kalkulator.java
@@ -9,23 +9,29 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JTextField;
 
-public class Kalkulator implements ActionListener{
+public class Kalkulator {
 
 	public static void main(String[] args) {
-		new Kalkulator();
+		Kalkulator k = new Kalkulator();
+		System.out.println(k.usunNiepotrzebneZera("3.0"));
+
 	}
 	
-	String sEkran = "0";
-	String[] sKey = {"1","2","3","C","4","5","6","+","7","8","9","-","0","*","/","="};
-	Color[] cKey = {Color.BLACK, Color.BLACK, Color.BLACK, Color.RED, Color.BLACK, Color.BLACK, Color.BLACK, Color.BLUE,
-			Color.BLACK, Color.BLACK, Color.BLACK, Color.BLUE, Color.BLACK, Color.BLUE, Color.BLUE, Color.GREEN};
-	long numer1, numer2;
-	boolean plus, minus, razy, dziel;
+String sEkran =null;
+String[] sKey = {"1","2","3","C","4","5","6","+","7","8","9","-","0","*","/","="};
 	
-	JFrame okno = new JFrame("Kalkulator");
-	JTextField text = new JTextField(sEkran);
-	JButton[] bKey = new JButton [16];
-	Font font = new Font ("System",Font.BOLD,15);
+Buttons[] numButtons = new NumericButton[10];
+Buttons[] calButtons = new ComputeButton[6];
+	
+	
+JFrame okno = new JFrame("Niezawodny kalkulator");
+JTextField text = new JTextField(sEkran);
+JButton[] bKey = new JButton [16];
+Font font = new Font ("System",Font.BOLD,15);
+	
+double whatWas;
+double whatIs;
+String znak=null;
 	
 	
 	Kalkulator(){
@@ -33,20 +39,30 @@ public class Kalkulator implements ActionListener{
 		for(byte i=0;i<16;i++) {
 			bKey[i]=new JButton(sKey[i]);
 			okno.add(bKey[i]);
-			bKey[i].addActionListener(this);
 		}
 		
 		byte index = 0;
 		for(byte y=0; y<4;y++) {
 			for(byte x=0;x<4;x++) {
 				bKey[index].setBounds(10+(x*50),55+(y*50),45,45);
-				bKey[index].setFont(font);
-				
-				bKey[index].setForeground(cKey[index]);
+				bKey[index].setFont(font);	
 				index++;
 			}
+			
 		}
 		
+		int j = 0, k = 0;
+		for(byte i = 0;i<16;i++) {
+			if((i%4==3)||i>12) {
+				calButtons[j]=new ComputeButton(sKey[i],bKey[i]);
+				j++;
+			}
+			else {
+				numButtons[k]=new NumericButton(sKey[i],bKey[i]);
+				k++;
+			}
+		}	
+			
 		text.setBounds(10, 10, 195, 35);
 		text.setFont(new Font("System",Font.BOLD,20));
 		text.setEditable(false);
@@ -58,139 +74,133 @@ public class Kalkulator implements ActionListener{
 		okno.setLocationRelativeTo(null); // at center
 		okno.setResizable(false);
 		okno.setLayout(null);
-		okno.setVisible(true); //have to be at the end
-		
-		
-	}
-	@Override
-	public void actionPerformed(ActionEvent e) {
-		// TODO Auto-generated method stub
-		Object cel = e.getSource()	;
-		for(byte i=0;i<16;i++) {
-			if(cel==bKey[i]) {
-				oblicz(i);
-			}
-		}
-	}
-	private void reset() {
-		plus = false;
-		minus = false;
-		razy = false;
-		dziel = false;
-		
+		okno.setVisible(true); 
 	}
 	
-	private void oblicz(int i) {
+class NumericButton implements Buttons, ActionListener{
 		
-		if(cKey[i]==Color.BLACK&& sEkran.length()<16) {
-			
-			if(sEkran=="0") sEkran=bKey[i].getText();
-			else sEkran+=bKey[i].getText();
-			
+		String sKey;
+		Color cKey;
+		JButton bKey;
+
+		public Color getColorValue() {
+			return cKey;
+		}
+
+		public NumericButton(String sKey,  JButton bKey) {
+			this.sKey = sKey;
+			this.cKey = Color.BLACK;
+			this.bKey = bKey;
+			bKey.setForeground(cKey);
+			bKey.addActionListener(this);
+		}
+
+		public void actionPerformed(ActionEvent e) {
+			addNumber(sKey);
+			}
+}
+
+class ComputeButton implements Buttons, ActionListener{
+		
+		String sKey;
+		Color cKey;
+		JButton bKey;
+		
+		public ComputeButton(String sKey,JButton bKey) {
+			super();
+			this.sKey = sKey;
+			this.cKey = Color.RED;
+			this.bKey = bKey;
+			bKey.setForeground(cKey);
+			bKey.addActionListener(this);
+		}
+		public String getsKey() {
+			return sKey;
+		}
+		public Color getColorValue() {
+			return cKey;
+		}
+		
+		public void actionPerformed(ActionEvent e) {
+			setZnak(sKey);	
+			}
+}
+
+	private void addNumber(String sKey) {
+		if(sEkran!=null&&sEkran.length()<16) 
+			 sEkran+=sKey;
+			else  sEkran=sKey;
+		
 		text.setText(sEkran);
+	}
+
+	void setZnak(String sKey) {
+		if(sKey=="C") {
+			znak=null;
+			sEkran=null;
+			text.setText(sEkran);
 		}
-		else if (i==3) {
-				sEkran="0";
-				reset();
-				text.setText(sEkran);
-		}// co jesli raz dodaje raz odejmuje, mieszane dzialania
-		else if(i==7){
-				
-			if(plus) {
-				numer2 = Long.parseLong(sEkran);
-				numer1+=numer2;
-				sEkran = String.valueOf(numer1);
-				text.setText(sEkran);
-				sEkran="0";
-				
-			}
-			else {
-				reset();
-				plus = true;
-				numer1 = Long.parseLong(sEkran);
-				sEkran="0";
-				
+		else if(sKey=="=") {
+			if(znak!=null&&sEkran!=null) {
+				whatIs=Double.parseDouble(sEkran);
+				oblicz();
+				text.setText(usunNiepotrzebneZera(sEkran));
+				znak=null;
+				whatWas=Double.parseDouble(sEkran);
+				sEkran=null;
 			}
 		}
-		else if(i==11){
+		else {	
+		 if(sEkran!=null) {
 			
-			if(minus) {
-				numer2 = Long.parseLong(sEkran);
-				numer1-=numer2;
-				sEkran = String.valueOf(numer1);
-				text.setText(sEkran);
-				sEkran="0";
-				
+			if(znak!=null) {
+				whatIs=Double.parseDouble(sEkran);
+				oblicz();
+				text.setText(usunNiepotrzebneZera(sEkran));
 			}
-			else {
-				reset();
-				minus = true;
-				numer1 = Long.parseLong(sEkran);
-				sEkran="0";
-				
-			}
+			whatWas=Double.parseDouble(sEkran);
+			sEkran=null;
+		 }
+		znak=sKey;
 		}
-		else if(i==13){
-			
-			if(razy) {
-				numer2 = Long.parseLong(sEkran);
-				numer1*=numer2;
-				sEkran = String.valueOf(numer1);
-				text.setText(sEkran);
-				sEkran="0";
-				
-			}
-			else {
-				reset();
-				razy = true;
-				numer1 = Long.parseLong(sEkran);
-				sEkran="0";
-				
-			}
-		}
-		else if(i==14){
-			
-			if(dziel) {
-				numer2 = Long.parseLong(sEkran);
-				numer1/=numer2;
-				sEkran = String.valueOf(numer1);
-				text.setText(sEkran);
-				sEkran="0";
-				
-			}
-			else {
-				reset();
-				dziel = true;
-				numer1 = Long.parseLong(sEkran);
-				sEkran="0";
-				
-			}
-		}
-		else if(cKey[i]==Color.GREEN){
-			
-			if(plus) {
-				numer2 = Long.parseLong(sEkran);
-				numer1+=numer2;
-			}
-			else if(minus) {
-				numer2 = Long.parseLong(sEkran);
-				numer1-=numer2;
-			}
-			else if(razy) {
-				numer2 = Long.parseLong(sEkran);
-				numer1*=numer2;
-			}
-			else if(dziel) {
-				numer2 = Long.parseLong(sEkran);
-				numer1/=numer2;
-			}
-				
-		sEkran = String.valueOf(numer1);
-		text.setText(sEkran);
-		sEkran="0";
-		}
-		
 	}
 	
+	 void oblicz() {  
+		 switch(znak){
+			 case "+":{
+				 whatWas+=whatIs; break;
+			 }
+			 case "-" :{
+				 whatWas-=whatIs; break;
+			 }
+			 case "/" : {
+				 whatWas=whatWas/whatIs; break;
+			 }
+			 case "*" : {
+				 whatWas*=whatIs; break;
+			 }	 
+		 }
+		 sEkran = zaokraglij(String.valueOf(whatWas)); 
+	 }
+	String zaokraglij(String x) {
+		if(x.length()>16)
+		{
+			int i =Integer.parseInt(x.substring(16,17));
+			
+			if(i>=5) x=x.substring(0, 15)+String.valueOf(Integer.parseInt(x.substring(15,16))+1);
+			
+			else x=x.substring(0, 16);
+		}
+		return x;
+	}	
 	
+	String usunNiepotrzebneZera(String x) {
+		int i = x.length();
+		while((x.contains(".")) && (x.charAt(i-1)=='0'||x.charAt(i-1)=='.')){
+				x=x.substring(0, i-1);
+				i = x.length();
+			}
+		
+		return x;
+	}	
 }
